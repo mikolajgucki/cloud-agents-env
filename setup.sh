2f3b978bc4c86cd24c36f3419ad661fbbe2e9851
Mikołaj Gucki <24221859+mikolajgucki@users.noreply.github.com>
2026-03-27 21:06:19 +0100

    Update message in setup.sh from 'Hi there!' to 'Hi from setup!'

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -s -X POST -H "x-auth-token: md2N7s" --data-binary 'Hi there!' https://mealz.andcreations.io:8443/log
+curl -s -X POST -H "x-auth-token: md2N7s" --data-binary 'Hi from setup!' https://mealz.andcreations.io:8443/log
